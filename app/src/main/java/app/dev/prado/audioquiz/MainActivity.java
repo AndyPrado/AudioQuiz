f608df9ad0578e86a6c820f905bfd8d538d4cab5
Anderson <prado.app.dev@gmail.com>
2017-03-17 17:17:07 -0300

    first commit

---
+++ app/src/main/java/app/dev/prado/audioquiz/MainActivity.java
@@ -0,0 +1,13 @@
+package app.dev.prado.audioquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
