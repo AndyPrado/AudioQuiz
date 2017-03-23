5414ca167cc155c81b7dec5508e3e1bd99ed264d
Anderson <prado.app.dev@gmail.com>
2017-03-23 19:31:28 -0300

    coding implements

--- app/src/main/java/app/dev/prado/audioquiz/activities/AudiosActivity.java
+++ app/src/main/java/app/dev/prado/audioquiz/activities/AudiosActivity.java
@@ -18,6 +18,9 @@ public class AudiosActivity extends AppCompatActivity {
     private ListView listView;
 
     private String tag;
+    private int audioId;
+    private int[] audios = new int[4];
+    private String currentQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,51 +39,113 @@ public class AudiosActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-               //TODO handle the item click
+                audioId = audios[position];
+                currentQuiz = quizzes[position];
+                openPlayer();
             }
         });
 
         //manage the tag received and load the correct picture to each tag
         Intent intent = getIntent();
         tag = intent.getStringExtra("TAG");
+        manageTag(tag);
+    }
+
+    public void manageTag(String tag){
 
         switch (tag) {
             case "c1 Adults":
                 banner.setImageResource(R.drawable.c1adults);
+                audios[0] = R.raw.q1_c1_adult;
+                audios[1] = R.raw.q2_c1_adult;
+                audios[2] = R.raw.q3_c1_adult;
+                audios[3] = R.raw.q4_c1_adult;
                 break;
             case "c2 Adults":
                 banner.setImageResource(R.drawable.c2adults);
+                audios[0] = R.raw.q1_c2_adults;
+                audios[1] = R.raw.q2_c2_adults;
+                audios[2] = R.raw.q3_c2_adults;
+                audios[3] = R.raw.q4_c2_adults;
                 break;
             case "c1 Teens":
                 banner.setImageResource(R.drawable.c1teens);
+                audios[0] = R.raw.q1_c1_teens;
+                audios[1] = R.raw.q2_c1_teens;
+                audios[2] = R.raw.q3_c1_teens;
+                audios[3] = R.raw.q4_c1_teens;
                 break;
             case "c2 Teens":
                 banner.setImageResource(R.drawable.c2teens);
+                audios[0] = R.raw.q1_c2_teens;
+                audios[1] = R.raw.q2_c2_teens;
+                audios[2] = R.raw.q3_c2_teens;
+                audios[3] = R.raw.q4_c2_teens;
                 break;
             case "s1 Adults":
                 banner.setImageResource(R.drawable.s1adults);
+                audios[0] = R.raw.q1_s1_adult;
+                audios[1] = R.raw.q2_s1_adult;
+                audios[2] = R.raw.q3_s1_adult;
+                audios[3] = R.raw.q4_s1_adult;
                 break;
             case "s2 Adults":
                 banner.setImageResource(R.drawable.s2adults);
+                audios[0] = R.raw.q1_s2_adult;
+                audios[1] = R.raw.q2_s2_adult;
+                audios[2] = R.raw.q3_s2_adult;
+                audios[3] = R.raw.q4_s2_adult;
                 break;
             case "s1 Teens":
                 banner.setImageResource(R.drawable.s1teens);
+                audios[0] = 0;
+                audios[1] = R.raw.q2_s1_teens;
+                audios[2] = R.raw.q3_s1_teens;
+                audios[3] = R.raw.q4_s1_teens;
                 break;
             case "s2 Teens":
                 banner.setImageResource(R.drawable.s2teens);
+                audios[0] = 0;
+                audios[1] = R.raw.q2_s2_teens;
+                audios[2] = R.raw.q3_s2_teens;
+                audios[3] = R.raw.q4_s2_teens;
                 break;
             case "a1 Adults":
                 banner.setImageResource(R.drawable.a1adults);
+                audios[0] = R.raw.q1_a1_adult;
+                audios[1] = R.raw.q2_a1_adult;
+                audios[2] = R.raw.q3_a1_adult;
+                audios[3] = R.raw.q4_a1_adult;
                 break;
             case "a2 Adults":
                 banner.setImageResource(R.drawable.a2adults);
+                audios[0] = R.raw.q1_a2_adult;
+                audios[1] = R.raw.q2_a2_adult;
+                audios[2] = R.raw.q3_a2_adult;
+                audios[3] = R.raw.q4_a2_adult;
                 break;
             case "a1 Teens":
                 banner.setImageResource(R.drawable.a1teens);
+                audios[0] = R.raw.q1_a1_teens;
+                audios[1] = R.raw.q2_a1_teens;
+                audios[2] = R.raw.q3_a1_teens;
+                audios[3] = R.raw.q4_a1_teens;
                 break;
             case "a2 Teens":
                 banner.setImageResource(R.drawable.a2teens);
+                audios[0] = R.raw.q1_a2_teens;
+                audios[1] = R.raw.q2_a2_teens;
+                audios[2] = R.raw.q3_a2_teens;
+                audios[3] = R.raw.q4_a2_teens;
                 break;
         }
+    }
+
+    public void openPlayer(){
+
+        Intent i = new Intent(AudiosActivity.this, PlayerActivity.class);
+        i.putExtra("AUDIO_FILE", audioId);
+        i.putExtra("TAG", currentQuiz);
+        startActivity(i);
     }
 }
