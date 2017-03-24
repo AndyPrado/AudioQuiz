60b2779d676be94f49c8077bb57cec7664b8134f
Anderson <prado.app.dev@gmail.com>
2017-03-24 15:53:23 -0300

    coding implements

--- app/src/main/java/app/dev/prado/audioquiz/activities/LoginActivity.java
+++ app/src/main/java/app/dev/prado/audioquiz/activities/LoginActivity.java
@@ -1,15 +1,60 @@
 package app.dev.prado.audioquiz.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import app.dev.prado.audioquiz.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText username;
+    private EditText password;
+    private Button btnLogin;
+
+    private FirebaseAuth firebaseAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.editTextLogin);
+        password = (EditText) findViewById(R.id.editTextPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+       checkLogin();
+
+    }
+
+    public void checkLogin(){
+        if(firebaseAuth.getCurrentUser() != null){
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+
+    public void loginUser(){
+        firebaseAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString()).
+                addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(i);
+                        }
+                    }
+                });
     }
 }
