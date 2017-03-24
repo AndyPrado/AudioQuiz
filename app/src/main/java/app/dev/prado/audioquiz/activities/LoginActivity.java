2b3a868f5ee1e52cf42f20953a48e8a5c60bf3dd
Anderson <prado.app.dev@gmail.com>
2017-03-24 16:35:32 -0300

    coding implements

--- app/src/main/java/app/dev/prado/audioquiz/activities/LoginActivity.java
+++ app/src/main/java/app/dev/prado/audioquiz/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -45,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void loginUser(){
+    public void loginUser(View v){
         firebaseAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString()).
                 addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
